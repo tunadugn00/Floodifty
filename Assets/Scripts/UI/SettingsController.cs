e34b57c8839d05202541526103831e64152188c8
Mai Tuấn Dũng <dungtuanmai@gmail.com>
2025-12-01 23:02:37 +0700

    update setting UI/UX

--- Assets/Scripts/UI/SettingsController.cs
+++ Assets/Scripts/UI/SettingsController.cs
@@ -1,13 +1,25 @@
+﻿using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
-    [SerializeField] private GameObject musicCrossLine;
-    [SerializeField] private GameObject sfxCrossLine;
+    [SerializeField] private RectTransform musicHandle;
+    [SerializeField] private RectTransform sfxHandle;
+
+    [SerializeField] private Image musicHandleImage;
+    [SerializeField] private Image sfxHandleImage;
+
+    [SerializeField] private float moveDuration = 0.2f; 
+    [SerializeField] private float handleOnX = 35f; 
+    [SerializeField] private float handleOffX = -35f;
+
+    [SerializeField] private Color onColor = Color.green;
+    [SerializeField] private Color offColor = Color.white;
 
     private void Start()
     {
-        RefreshUI();
+        UpdateVisuals(false);
         SoundManager.Instance.PlayMusic();
     }
 
@@ -16,19 +28,49 @@ public class SettingsController : MonoBehaviour
         bool enabled = !SaveSystem.IsMusicEnabled();
         SaveSystem.SetMusicEnabled(enabled);
         SoundManager.Instance.PlayMusic();
-        RefreshUI();
+        UpdateVisuals(true);
         SoundManager.Instance.PlayClick();
     }
     public void ToggleSFX()
     {
         bool enabled = !SaveSystem.IsSFXEnabled();
         SaveSystem.SetSFXEnabled(enabled);
-        RefreshUI();
+        UpdateVisuals(true);
         SoundManager.Instance.PlayClick();
     }
-    private void RefreshUI()
+    private void UpdateVisuals(bool animate)
     {
-        if (musicCrossLine) musicCrossLine.SetActive(!SaveSystem.IsMusicEnabled());
-        if (sfxCrossLine) sfxCrossLine.SetActive(!SaveSystem.IsSFXEnabled());
+        // --- Music ---
+        bool musicOn = SaveSystem.IsMusicEnabled();
+        float targetMusicX = musicOn ? handleOnX : handleOffX;
+        Color targetMusicColor = musicOn ? onColor : offColor;
+
+        if (animate)
+        {
+            musicHandle.DOAnchorPosX(targetMusicX, moveDuration).SetEase(Ease.OutBack);
+            // Hiệu ứng đổi màu 
+            if (musicHandleImage) musicHandleImage.DOColor(targetMusicColor, moveDuration);
+        }
+        else
+        {
+            musicHandle.anchoredPosition = new Vector2(targetMusicX, musicHandle.anchoredPosition.y);
+            if (musicHandleImage) musicHandleImage.color = targetMusicColor;
+        }
+
+        // --- SFX ---
+        bool sfxOn = SaveSystem.IsSFXEnabled();
+        float targetSfxX = sfxOn ? handleOnX : handleOffX;
+        Color targetSfxColor = sfxOn ? onColor : offColor;
+
+        if (animate)
+        {
+            sfxHandle.DOAnchorPosX(targetSfxX, moveDuration).SetEase(Ease.OutBack);
+            if (sfxHandleImage) sfxHandleImage.DOColor(targetSfxColor, moveDuration);
+        }
+        else
+        {
+            sfxHandle.anchoredPosition = new Vector2(targetSfxX, sfxHandle.anchoredPosition.y);
+            if (sfxHandleImage) sfxHandleImage.color = targetSfxColor;
+        }
     }
 }
